a2d296b02b753da8b8d240a6acb591991ebda7fb
Brad Slagle <btslagle@gmail.com>
2022-06-10 20:21:35 -0500

    intial commit

--- src/main/java/com/bslagle/easyxferapi/vehicle/VehicleController.java
+++ src/main/java/com/bslagle/easyxferapi/vehicle/VehicleController.java
@@ -23,59 +23,59 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 @RequestMapping("vehicles")
 public class VehicleController {
-  @Autowired
-  private VehicleService vehicleService;
+    @Autowired
+    private VehicleService vehicleService;
 
-  @GetMapping
-  @ResponseStatus(HttpStatus.OK)
-  public Map<String, Iterable<Vehicle>> list(){
-    Iterable<Vehicle> vehicles = vehicleService.list();
-    return createHashPlural(vehicles);
-  }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Iterable<Vehicle>> list() {
+        Iterable<Vehicle> vehicles = vehicleService.list();
+        return createHashPlural(vehicles);
+    }
 
-  @GetMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  public Map<String, Vehicle> read(@PathVariable Long id) {
-    Vehicle vehicle = vehicleService
-      .findById(id)
-      .orElseThrow(() -> new ResourceNotFoundException("No vehicle with that ID"));
-    return createHashSingular(vehicle);
-  }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Vehicle> read(@PathVariable Long id) {
+        Vehicle vehicle = vehicleService
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No vehicle with that ID"));
+        return createHashSingular(vehicle);
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Map<String, Vehicle> create(@Validated @RequestBody Vehicle vehicle) {
-    Vehicle createdVehicle = vehicleService.create(vehicle);
-    return createHashSingular(createdVehicle);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Map<String, Vehicle> create(@Validated @RequestBody Vehicle vehicle) {
+        Vehicle createdVehicle = vehicleService.create(vehicle);
+        return createHashSingular(createdVehicle);
+    }
 
-  @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Map<String, Vehicle> update(@RequestBody Vehicle vehicle, @PathVariable Long id) {
-    Vehicle updatedVehicle = vehicleService
-      .update(vehicle)
-      .orElseThrow(() -> new ResourceNotFoundException("No vehicle with that ID"));
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Map<String, Vehicle> update(@RequestBody Vehicle vehicle, @PathVariable Long id) {
+        Vehicle updatedVehicle = vehicleService
+                .update(vehicle)
+                .orElseThrow(() -> new ResourceNotFoundException("No vehicle with that ID"));
 
-    return createHashSingular(updatedVehicle);
-  }
+        return createHashSingular(updatedVehicle);
+    }
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void delete(@PathVariable Long id) {
-    vehicleService.deleteById(id);
-  }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        vehicleService.deleteById(id);
+    }
 
-  private Map<String, Vehicle> createHashSingular(Vehicle vehicle){
-    Map<String, Vehicle> response = new HashMap<String, Vehicle>();
-    response.put("vehicle", vehicle);
+    private Map<String, Vehicle> createHashSingular(Vehicle vehicle) {
+        Map<String, Vehicle> response = new HashMap<String, Vehicle>();
+        response.put("vehicle", vehicle);
 
-    return response;
-  }
+        return response;
+    }
 
-  private Map<String, Iterable<Vehicle>> createHashPlural(Iterable<Vehicle> vehicles){
-    Map<String, Iterable<Vehicle>> response = new HashMap<String, Iterable<Vehicle>>();
-    response.put("vehicles", vehicles);
+    private Map<String, Iterable<Vehicle>> createHashPlural(Iterable<Vehicle> vehicles) {
+        Map<String, Iterable<Vehicle>> response = new HashMap<String, Iterable<Vehicle>>();
+        response.put("vehicles", vehicles);
 
-    return response;
-  }
+        return response;
+    }
 }
--- src/main/java/com/bslagle/easyxferapi/vehicle/VehicleService.java
+++ src/main/java/com/bslagle/easyxferapi/vehicle/VehicleService.java
@@ -7,44 +7,44 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleService {
-  @Autowired
-  private VehicleRepository vehicleRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
-  public Iterable<Vehicle> list(){
-    return vehicleRepository.findAll();
-  }
+    public Iterable<Vehicle> list() {
+        return vehicleRepository.findAll();
+    }
 
-  public Optional<Vehicle> findById(Long id){
-    return vehicleRepository.findById(id);
-  }
+    public Optional<Vehicle> findById(Long id) {
+        return vehicleRepository.findById(id);
+    }
 
-  public Vehicle create(Vehicle vehicle) {
-    return vehicleRepository.save(vehicle);
-  }
+    public Vehicle create(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
 
-  public Optional<Vehicle> update(Vehicle vehicle) {
-    Optional<Vehicle> foundVehicle = vehicleRepository.findById(vehicle.getId());
+    public Optional<Vehicle> update(Vehicle vehicle) {
+        Optional<Vehicle> foundVehicle = vehicleRepository.findById(vehicle.getId());
 
-    if (foundVehicle.isPresent()) {
-        Vehicle updatedVehicle = foundVehicle.get();
-        updatedVehicle.setImageUrl(vehicle.getImageUrl());
-        updatedVehicle.setMake(vehicle.getMake());
-        updatedVehicle.setModel(vehicle.getModel());
-        updatedVehicle.setYear(vehicle.getYear());
-        updatedVehicle.setCondition(vehicle.getCondition());
-        updatedVehicle.setTerm(vehicle.getTerm());
-        updatedVehicle.setMonthly(vehicle.getMonthly());
-        updatedVehicle.setMileage(vehicle.getMileage());
-        updatedVehicle.setLocation(vehicle.getLocation());
+        if (foundVehicle.isPresent()) {
+            Vehicle updatedVehicle = foundVehicle.get();
+            updatedVehicle.setImageUrl(vehicle.getImageUrl());
+            updatedVehicle.setMake(vehicle.getMake());
+            updatedVehicle.setModel(vehicle.getModel());
+            updatedVehicle.setYear(vehicle.getYear());
+            updatedVehicle.setCondition(vehicle.getCondition());
+            updatedVehicle.setTerm(vehicle.getTerm());
+            updatedVehicle.setMonthly(vehicle.getMonthly());
+            updatedVehicle.setMileage(vehicle.getMileage());
+            updatedVehicle.setLocation(vehicle.getLocation());
 
-        vehicleRepository.save(updatedVehicle);
-        return Optional.of(updatedVehicle);
-      } else {
-        return Optional.empty();
-      }
-  }
+            vehicleRepository.save(updatedVehicle);
+            return Optional.of(updatedVehicle);
+        } else {
+            return Optional.empty();
+        }
+    }
 
-  public void deleteById(Long id) {
-    vehicleRepository.deleteById(id);
-  }
+    public void deleteById(Long id) {
+        vehicleRepository.deleteById(id);
+    }
 }
