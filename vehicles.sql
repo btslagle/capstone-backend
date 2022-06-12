a0cf61caad633466635cbd13beb382606c43d275
Brad Slagle <btslagle@gmail.com>
2022-06-12 12:17:26 -0500

    Create database

---
+++ vehicles.sql
@@ -0,0 +1,4 @@
+TRUNCATE vehicle;
+
+INSERT INTO vehicle (id, image_url, make, model, year, condition, term, monthly,mileage, location) VALUES (1, 'https://www.ford.com/is/image/content/dam/brand_ford/en_us/brand/trucks/f150/brand_f-150/dm/22_F150_R7A7574_Tremor_CAN.tif?croppathe=1_21x9&wid=1440',
+'Ford', 'F150', '2022', 'Great', '32', '$565.93', '12,000', 'Tyler, TX 75702');
