e7c9d685b2d4a94314cbed32c815d31db1e31ca7
Brad Slagle <btslagle@gmail.com>
2022-06-13 17:26:45 -0500

    Update .sql

--- vehicles.sql
+++ vehicles.sql
@@ -2,7 +2,7 @@ TRUNCATE vehicle;
 
 INSERT INTO vehicle (id, image_url, make, model, year, condition, term, monthly,mileage, location) VALUES (3000, 'https://www.ford.com/is/image/content/dam/brand_ford/en_us/brand/trucks/f150/brand_f-150/dm/22_F150_R7A7574_Tremor_CAN.tif?croppathe=1_21x9&wid=1440',
 'Ford', 'F150', '2022', 'Great', '32', '$565.93', '12,000', 'Tyler, TX 75702');
-INSERT INTO vehicle (id, image_url, make, model, year, condition, term, monthly,mileage, location) VALUES 3001, 'https://i.gaw.to/content/photos/48/41/484110-ford-bronco-2021-le-vrai.jpeg',
+INSERT INTO vehicle (id, image_url, make, model, year, condition, term, monthly,mileage, location) VALUES (3001, 'https://i.gaw.to/content/photos/48/41/484110-ford-bronco-2021-le-vrai.jpeg',
 'Ford', 'Bronco', '2022', 'Great', '63', '$785.93', '27,000', 'Denver, CO 80014');
 INSERT INTO vehicle (id, image_url, make, model, year, condition, term, monthly,mileage, location) VALUES (3002, 'https://www.carscoops.com/wp-content/uploads/2022/01/Ford-Mustang-Shelby-GT350-1024x555.jpg',
 'Ford', 'Mustang', '2021', 'Good', '15', '$1273.89', '28,000', 'Boston, MA 02108');
